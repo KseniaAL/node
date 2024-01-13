60748d6997ce778dfa0c251da1a62d96a11e720a
KseniaAL <keisyowl21@gmail.com>
2024-01-13 15:24:20 +0300

    Подключение к БД

---
+++ mysql.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+connection = mysql.createConnection({
+    host: "localhost",
+    user:"root",
+    database: "article",
+    password: ""
+}).promise();
+
+connection.connect(function (err){
+    if (err){
+        return console.error("Error: "+err.message);
+    }
+    else{
+        console.log("подключение к серверу MySQL успешно установлено")
+    }
+});
+module.exports = connection;
